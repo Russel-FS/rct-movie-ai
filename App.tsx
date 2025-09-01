42c5608edc3e2dbbe67e5662fc92e12b1d1a8291
Sahel-pl <u00030737@sise.com.pe>
2025-09-01 14:55:59 -0500

    Footer add

--- App.tsx
+++ App.tsx
@@ -1,33 +1,49 @@
-import { ScreenContent } from 'components/ScreenContent';
+import { ScreenContent } from 'src/shared/components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import Animated, { useSharedValue, useAnimatedStyle, withSpring } from 'react-native-reanimated';
+import { Text, View, StyleSheet } from 'react-native';
+import { Footer } from './src/home/components/Footer';
 
 import './global.css';
 
+// Marcadores de posición para los otros componentes
+const Header = () => (
+  <View style={styles.header}>
+    <Text style={styles.placeholderText}>Header</Text>
+  </View>
+);
+const Hero = () => (
+  <View style={styles.hero}>
+    <Text style={styles.placeholderText}>Hero Section</Text>
+  </View>
+);
+
 export default function App() {
-  const scale = useSharedValue(1);
-
-  const animatedStyle = useAnimatedStyle(() => {
-    return {
-      transform: [{ scale: scale.value }],
-    };
-  });
-
-  const startAnimation = () => {
-    scale.value = withSpring(1.2, {}, () => {
-      scale.value = withSpring(1);
-    });
-  };
-
   return (
     <>
       <ScreenContent title="Home" path="App.tsx">
-        <Animated.View style={[{ padding: 20 }, animatedStyle]}>
-          <Text onPress={startAnimation}>¡Toca este texto para ver la animación!</Text>
-        </Animated.View>
+        <Header />
+        <Hero />
       </ScreenContent>
+      <Footer />
       <StatusBar style="auto" />
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    padding: 15,
+    backgroundColor: '#2C2C2E',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  hero: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  placeholderText: {
+    color: 'white',
+    fontSize: 18,
+  },
+});

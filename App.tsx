8a4bc859b8f8990b956a31b18d0c43c5f5702f7c
Russel-Fs <russelfloressolano900@gmail.com>
2025-09-01 21:08:34 -0500

    update packages

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
 import Animated, { useSharedValue, useAnimatedStyle, withSpring } from 'react-native-reanimated';
 
-import { ScreenContent } from '~/shared/components/ScreenContent';
 import './global.css';
+import { Container } from '~/shared/components/Container';
 
 export default function App() {
   const scale = useSharedValue(1);
@@ -22,11 +22,13 @@ export default function App() {
 
   return (
     <>
-      <ScreenContent title="Home" path="App.tsx">
+      <Container>
         <Animated.View style={[{ padding: 20 }, animatedStyle]}>
-          <Text onPress={startAnimation}>¡Toca este texto para ver la animación!</Text>
+          <Text className="rounded-full bg-blue-600 p-3 text-white" onPress={startAnimation}>
+            ¡Toca este texto para ver la animación!
+          </Text>
         </Animated.View>
-      </ScreenContent>
+      </Container>
       <StatusBar style="auto" />
     </>
   );

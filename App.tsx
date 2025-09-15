a1f47304fe5cb5523108031a29ba803f0520f33c
Russel-Fs <russelfloressolano900@gmail.com>
2025-09-14 22:49:10 -0500

    feat(home): add navigation component to app layout
    Add new Navigation component and integrate it into the main App component to provide site-wide navigation functionality.

--- App.tsx
+++ App.tsx
@@ -3,12 +3,14 @@ import { StatusBar } from 'expo-status-bar';
 import './global.css';
 import { Container } from '~/shared/components/Container';
 import Home from '~/home/page/Home';
+import Navigation from '~/home/components/Navigation';
 
 export default function App() {
   return (
     <>
       <Container>
         <Home />
+        <Navigation />
       </Container>
       <StatusBar style="auto" />
     </>
